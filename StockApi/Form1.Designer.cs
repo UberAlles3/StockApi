0471352bc252e10d6c7e9e64366a255bcd55c892
uberalles <uberalles@DESKTOP-F70118R>
2023-08-18 22:29:17 -0500

    changhed apis

--- StockApi/Form1.Designer.cs
+++ StockApi/Form1.Designer.cs
@@ -29,34 +29,71 @@ namespace StockApi
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnGetOne = new System.Windows.Forms.Button();
+            this.txtStockTicker = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGetAll = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnGetOne
             // 
-            this.button1.Location = new System.Drawing.Point(376, 212);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Get movie info";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnGetOne.Location = new System.Drawing.Point(33, 115);
+            this.btnGetOne.Name = "btnGetOne";
+            this.btnGetOne.Size = new System.Drawing.Size(161, 23);
+            this.btnGetOne.TabIndex = 0;
+            this.btnGetOne.Text = "Get individual stock data";
+            this.btnGetOne.UseVisualStyleBackColor = true;
+            this.btnGetOne.Click += new System.EventHandler(this.btnGetOne_click);
+            // 
+            // txtStockTicker
+            // 
+            this.txtStockTicker.BackColor = System.Drawing.SystemColors.Window;
+            this.txtStockTicker.Location = new System.Drawing.Point(80, 86);
+            this.txtStockTicker.Name = "txtStockTicker";
+            this.txtStockTicker.Size = new System.Drawing.Size(114, 23);
+            this.txtStockTicker.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ticker:";
+            // 
+            // btnGetAll
+            // 
+            this.btnGetAll.Location = new System.Drawing.Point(33, 176);
+            this.btnGetAll.Name = "btnGetAll";
+            this.btnGetAll.Size = new System.Drawing.Size(161, 23);
+            this.btnGetAll.TabIndex = 3;
+            this.btnGetAll.Text = "Get all stock data";
+            this.btnGetAll.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGetAll);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStockTicker);
+            this.Controls.Add(this.btnGetOne);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGetOne;
+        private System.Windows.Forms.TextBox txtStockTicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGetAll;
     }
 }
 
--- StockApi/Form1.cs
+++ StockApi/Form1.cs
@@ -12,6 +12,7 @@ using System.Windows.Forms;
 using System.Net.Http.Headers;
 using System.Net.Http;
 using System.Diagnostics;
+using Yahoo.Finance;
 
 // 2dadcc0b -- API key
 
@@ -19,41 +20,112 @@ namespace StockApi
 {
     public partial class Form1 : Form
     {
+        private static StockData _StockData;
+
         public Form1()
         {
             InitializeComponent();
         }
 
 
-        private async void button1_Click(object sender, EventArgs e)
+        private async void btnGetOne_click(object sender, EventArgs e)
         {
-            var client = new HttpClient();
-            var request = new HttpRequestMessage
+            EquitySummaryData equitySummaryData = new EquitySummaryData();
+
+            string url = "https://finance.yahoo.com/quote/intc";
+            HttpClient cl = new HttpClient();
+            HttpResponseMessage hrm = await cl.GetAsync(url);
+            string web = await hrm.Content.ReadAsStringAsync();
+
+            equitySummaryData.Beta =  System.Convert.ToSingle(GetDataByDataTestName(web, "BETA_5Y-value"));
+
+
+
+            
+
+            string jsonResponse = "";
+            
+            if(string.IsNullOrEmpty(txtStockTicker.Text))
+            {
+                MessageBox.Show("Enter a valid stock ticker.");
+                return;
+            }
+            
+            HttpRequestMessage request = CreateHttpRequest(txtStockTicker.Text);
+            await GetHttpResponse(request, jsonResponse);
+        }
+
+        private static HttpRequestMessage CreateHttpRequest(string stockTicker)
+        {
+            return new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://yh-finance-complete.p.rapidapi.com/yhf?ticker=pypl"),
+                RequestUri = new Uri("https://yh-finance-complete.p.rapidapi.com/yhf?ticker=" + stockTicker),
                 Headers =
                 {
                     { "X-RapidAPI-Key", "5b43e47e81msh209d3a319f60d02p1e399ajsne949d592c311" },
                     { "X-RapidAPI-Host", "yh-finance-complete.p.rapidapi.com" },
                 },
             };
+        }
 
-            await GetHttpResponse(client, request);
-         }
-
-        private static async Task GetHttpResponse(HttpClient client, HttpRequestMessage request)
+        private static async Task GetHttpResponse(HttpRequestMessage request, string jsonReponse)
         {
+            var client = new HttpClient();
+
             using (var response = client.SendAsync(request))
             {
-                var body = await response.Result.Content.ReadAsStringAsync();
-                dynamic stock = JsonConvert.DeserializeObject(body);
-                decimal beta = 0;
-                bool success = decimal.TryParse(stock.summaryDetail.beta.ToString(), out beta);
-
-                ///
-                Debug.WriteLine(beta);
+                string jsonResponse = await response.Result.Content.ReadAsStringAsync();
+                ParseJsonResponse(jsonResponse);
             }
+        }
+
+        private static void ParseJsonResponse(string body)
+        {
+            dynamic stock = JsonConvert.DeserializeObject(body);
+
+            try
+            {
+                string error = stock.error;
+                if (error != null)
+                {
+                    MessageBox.Show("API Error:" + Environment.NewLine + error + Environment.NewLine + "Possibly an invalid ticker.");
+                    return;
+                }
+            }
+            catch
+            {
+            }
+
+            decimal beta = 0;
+            bool success = decimal.TryParse(stock.summaryDetail.beta.ToString(), out beta);
+
+            if (success)
+            {
+                _StockData = new StockData();
+                _StockData.Beta = beta;
+            }
+
+
+            Debug.WriteLine(beta);
+        }
+
+        private string GetDataByDataTestName(string web_data, string data_test_name)
+        {
+            int loc1 = 0;
+            int loc2 = 0;
+
+            loc1 = web_data.IndexOf("data-test=\"" + data_test_name + "\"");
+            if (loc1 == -1)
+            {
+                throw new Exception("Unable to find data with data test name '" + web_data + "' inside web data.");
+            }
+
+            loc1 = web_data.IndexOf(">", loc1 + 1);
+            loc2 = web_data.IndexOf("<", loc1 + 1);
+
+            string middle = web_data.Substring(loc1 + 1, loc2 - loc1 - 1);
+            return middle;
         }
     }
 }
