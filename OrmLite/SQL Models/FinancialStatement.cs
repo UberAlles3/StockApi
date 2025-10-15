59ee6e14792d662030f9fce86ab9cace2e744e4f
Gary Drake <drakerama@yahoo.com>
2025-10-15 14:30:33 -0500

    Adding Financialstatement

---
+++ OrmLite/SQL Models/FinancialStatement.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
+using ServiceStack.OrmLite;
+
+
+namespace OrmLite.SQL_Models
+{
+    public class FinancialStatement
+    {
+        /// <summary>
+        /// Get the database connection
+        /// </summary>
+        /// <returns></returns>
+        public static OrmLiteConnectionFactory FinancialStatementFactory()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["StockDataConnection"].ConnectionString;
+            OrmLiteConnectionFactory factory = new OrmLiteConnectionFactory(connectionString, SqlServerDialect.Provider);
+            return factory;
+        }
+        public CashFlow CashFlow { get; set; }
+        //public BalanceSheet BalanceSheet { get; set; }
+        //public IncomeStatement IncomeStatement { get; set; }
+
+        #region Pass-Through Properties
+        //TODO
+
+
+        //public string Ticker
+        //{
+        //    get
+        //    {
+        //        return this.BalanceSheet.Ticker;
+        //    }
+        //    set
+        //    {
+        //        this.BalanceSheet.Ticker = value;
+        //        this.IncomeStatement.Ticker = value;
+        //        this.CashFlow.Ticker = value;
+        //    }
+        //}
+
+        //public DateTime StartDate
+        //{
+        //    get
+        //    {
+        //        return this.BalanceSheet.StartDate;
+        //    }
+        //    set
+        //    {
+        //        this.BalanceSheet.StartDate = value;
+        //        this.IncomeStatement.StartDate = value;
+        //        this.CashFlow.StartDate = value;
+        //    }
+        //}
+
+        //public DateTime EndDate
+        //{
+        //    get
+        //    {
+        //        return this.BalanceSheet.EndDate;
+        //    }
+        //    set
+        //    {
+        //        this.BalanceSheet.EndDate = value;
+        //        this.IncomeStatement.EndDate = value;
+        //        this.CashFlow.EndDate = value;
+        //    }
+        //}
+
+        #endregion
+
+        public FinancialStatement()
+        {
+            //this.IncomeStatement = new IncomeStatement();
+            this.CashFlow = new CashFlow();
+        }
+
+        public void SaveCashFlow()
+        {
+            Debug.WriteLine("SaveCashFlow()");
+
+            var factory = FinancialStatementFactory();
+
+            using (IDbConnection db = factory.OpenDbConnection())
+            {
+                db.CreateTableIfNotExists<CashFlow>();
+
+                db.Delete<CashFlow>(x => x.StartDate > DateTime.Now.AddYears(-5));
+
+                //foreach (string key in _statementCache.Keys)
+                //{
+                CashFlow cashFlow = new CashFlow() { StartDate = DateTime.Now.Date, EndDate = DateTime.Now.Date, EndCashPosition = 111, FreeCashFlow = 222.222, Ticker = "AAPL", OperatingCashFlow = 555 };
+                db.Insert<CashFlow>(cashFlow);
+                //    db.Insert<BalanceSheet>(_statementCache[key].BalanceSheet);
+                //    db.Insert<CashFlow>(_statementCache[key].CashFlow);
+                //}
+            }
+        }
+    }
+}
