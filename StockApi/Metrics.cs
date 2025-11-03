045f7cbf3d90df3a795276be22c6338ad64c5708
Gary Drake <drakerama@yahoo.com>
2025-11-03 14:30:50 -0600

    HGHG

--- StockApi/Metrics.cs
+++ StockApi/Metrics.cs
@@ -58,8 +58,8 @@ namespace StockApi
             stockList = _excelManager.GetStockListFromPositionsTable(positionsDataTable);
             if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)
             {
-                stockList = stockList.Skip(0).Take(4).ToList();
-                //stockList = stockList.Skip(0).Take(30).ToList();
+                //stockList = stockList.Skip(80).Take(50).ToList();
+                stockList = stockList.Skip(0).Take(30).ToList();
                 desktopPath = Path.Combine(desktopPath, "StockMetricsMonday.txt");
             }
 
